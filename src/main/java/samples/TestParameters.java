62fec743520c0c03877f37a126967c378189ee6e
Oscar Orozco <oorozco@tiempodevelopment.com>
2018-05-16 09:21:29 -0500

    Added a couple of more libraries because the appium ones were missin so i added code to the samplefolder with the libriries for iOS

---
+++ src/main/java/samples/TestParameters.java
@@ -0,0 +1,20 @@
+package samples;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class TestParameters{
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities d = new DesiredCapabilities();
+        d.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
+        d.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        d.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        d.setCapability(MobileCapabilityType.APP, "/Users/oscar orozco/Library/Developer/Xcode/DerivedData/Rousesales-cwuiotcndhvhofaaofegozkoskdb/Build/Products/QA-iphonesimulator/Rouse Sales.app");
+        IOSDriver<IOSElement> driver = new IOSDriver<> (new URL("http://127.0.0.1:4723/wd/hub"), d);
+        driver.quit();
+    }
+
+}
